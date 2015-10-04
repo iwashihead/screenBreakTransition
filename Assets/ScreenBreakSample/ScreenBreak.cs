737f8dc16d2f2528bce7e6b936ae69a4e8705de1
iwashihead <h_tachihara@hotmail.co.jp>
2015-10-05 03:00:32 +0900

    init commit.

---
+++ Assets/ScreenBreakSample/ScreenBreak.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// スクリーンが割れるようなトランジションを表示.
+/// </summary>
+public class ScreenBreak : MonoBehaviour {
+	[SerializeField] private Animation breakAnim;
+	[SerializeField] private List<Camera> cameras;
+	[SerializeField] private float scale = 1f;
+	[SerializeField] private RenderTexture renderTex;
+	[SerializeField] private Material renderMaterial;
+	[SerializeField] private Camera targetCamera;
+
+	private bool _playFlag;
+	private bool _targetCamEnabled;
+
+
+	void Awake () {
+		if (renderTex == null) {
+			renderTex = new RenderTexture(
+				(int)(Screen.width / scale),
+				(int)(Screen.height / scale), 24);
+			renderTex.enableRandomWrite = false;
+		}
+		_playFlag = false;
+		gameObject.SetActive(false);
+	}
+
+	/// <summary>
+	/// スクリーン破壊演出の開始.
+	/// </summary>
+	[ContextMenu("Play")]
+	public void Play()
+	{
+		if (_playFlag) return;
+
+		_playFlag = true;
+
+		gameObject.SetActive(true);
+		StartCoroutine(co_Play());
+	}
+
+
+	IEnumerator co_Play()
+	{
+		if (_playFlag == false) yield break;
+
+		_targetCamEnabled = targetCamera.enabled;
+		targetCamera.enabled = true;
+
+		// カメラにレンダラテクスチャをセット.
+		foreach (var cam in cameras)
+			cam.targetTexture = renderTex;
+		
+		renderMaterial.mainTexture = renderTex;
+		breakAnim.Play("breaks");
+
+
+		// 1フレ待機.
+		yield return null;
+
+
+		// カメラにレンダラテクスチャをリセット.
+		foreach (var cam in cameras)
+			cam.targetTexture = null;
+
+
+		// アニメーション終わるまで待機
+		yield return new WaitForSeconds(breakAnim["breaks"].length);
+
+
+		// 後処理
+		_playFlag = false;
+		gameObject.SetActive(false);
+		targetCamera.enabled = _targetCamEnabled;
+	}
+}
---
+++ Assets/ScreenBreakSample/ScreenBreakController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenBreakController : MonoBehaviour {
+
+	public ScreenBreak screenBreak;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(2f);
+
+		screenBreak.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			screenBreak.Play();
+		}
+	}
+}
